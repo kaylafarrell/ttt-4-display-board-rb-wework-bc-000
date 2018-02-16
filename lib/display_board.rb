79edf2301ace746da147fefacebff926add45164
Kayla Farrell <kayla.farrell@wework.com>
2018-02-16 00:38:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+  row = "   |   |   "
+  divider = "-----------"
+  puts row
+  puts divider
+  puts row
+  puts divider
+  puts row
+end
+display_board
